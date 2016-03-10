6e1d6669584158253b17e2fda32e29fc382c530e
alin <alinvelican@gmail.com>
2016-03-11 00:22:59 +0200

    cout added

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "cza";
+	system("pause");
+    return 0;
+	
+}
+
